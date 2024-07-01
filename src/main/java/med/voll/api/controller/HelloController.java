798fcd7e5aef7df1f3288470226c2f8d266d3bfd
Joao <jhznn.andrade@gmail.com>
2024-07-01 17:07:16 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    public String olaMundo(){
+        return "Hello World Spring!";
+    }
+
+
+}
